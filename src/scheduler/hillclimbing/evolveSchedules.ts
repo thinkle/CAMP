ccc47a10b912bc610aad44148579861cdf3b61a3
Tom Hinkle <tmhinkle@gmail.com>
2025-01-29 19:53:33 -0500

    --dead code

--- src/scheduler/hillclimbing/evolveSchedules.ts
+++ src/scheduler/hillclimbing/evolveSchedules.ts
@@ -11,110 +11,16 @@ import type {
 import { improveSchedule } from "./improveSchedule";
 import { createScheduleInfo } from "./scheduleInfo";
 
-export const evolveSchedules = (
-  population: ScheduleInfo[],
-  studentPreferences: StudentPreferences[],
-  activities: Activity[],
-  noffspring = 10
-): ScheduleInfo[] => {
-  population.sort((a, b) => b.score - a.score);
-  // Select the top 25% of the population
-  const top = population.slice(
-    0,
-    Math.max(10, Math.ceil(population.length / 4))
-  );
-  for (let i = 0; i < noffspring; i++) {
-    let parentOne: ScheduleInfo = top[Math.floor(Math.random() * top.length)];
-    let parentTwo: ScheduleInfo = top[Math.floor(Math.random() * top.length)];
-    try {
-      let p1 = improveSchedule(
-        parentOne.schedule,
-        studentPreferences,
-        activities
-      );
-      let p2 = improveSchedule(
-        parentTwo.schedule,
-        studentPreferences,
-        activities
-      );
-      let childSchedule = mergeSchedules(
-        [p1, p2],
-        studentPreferences,
-        activities
-      );
-      childSchedule = improveSchedule(
-        childSchedule,
-        studentPreferences,
-        activities,
-        10
-      );
-
-      const childScheduleInfo = createScheduleInfo(
-        childSchedule,
-        studentPreferences,
-        activities,
-        "merge+" + parentOne.alg + "+" + parentTwo.alg,
-        parentOne.generation + 1
-      );
-      if (!population.find((s) => s.id === childScheduleInfo.id)) {
-        population.push(childScheduleInfo);
-        console.log(
-          "Added offspring to population, score ",
-          childScheduleInfo.score
-        );
-      } else {
-        console.log("Generated dup!");
-      }
-    } catch (e) {
-      console.log("Stillborn schedule...");
-    }
-  }
-  return population;
-};
-
-export const improveSchedules = (
-  population: ScheduleInfo[],
-  studentPreferences: StudentPreferences[],
-  activities: Activity[]
-): ScheduleInfo[] => {
-  // choose top 10% of population
-  population.sort((a, b) => b.score - a.score);
-  let top10 = population.slice(
-    0,
-    Math.max(10, Math.ceil(population.length / 10))
-  );
-  if (top10.length > 10) {
-    // pick 10 at random from top 10%;
-    top10 = top10.sort(() => Math.random() - 0.5).slice(0, 10);
-  }
-  for (let sched of top10) {
-    let newSchedule = improveSchedule(
-      sched.schedule,
-      studentPreferences,
-      activities,
-      10
-    );
-    let newInfo = createScheduleInfo(
-      newSchedule,
-      studentPreferences,
-      activities,
-      sched.alg + "i",
-      sched.generation + 1
-    );
-    if (newInfo.score > sched.score) {
-      console.log("Improved one", newInfo.score, newInfo);
-      population.push(newInfo);
-    }
-  }
-  return population;
-};
-
 export function* createCrosses(
   population: ScheduleInfo[],
   studentPreferences: StudentPreferences[],
   activities: Activity[],
-  rounds: number
+  rounds: number,
+  existingSet: Set<string> | null
 ) {
+  if (!existingSet) {
+    existingSet = new Set(population.map((s) => s.id));
+  }
   let combinations = getCombinations(population, 2);
   for (let pair of combinations) {
     try {
@@ -133,7 +39,13 @@ export function* createCrosses(
         mergedAlgName,
         pair[0].generation + 1
       );
-      yield mergedInfo;
+      if (existingSet.has(mergedInfo.id)) {
+        console.log("crossbreed ignoring dup");
+        continue;
+      } else {
+        existingSet.add(mergedInfo.id);
+        yield mergedInfo;
+      }
     } catch (err) {
       console.log("Error merging", err);
     }
